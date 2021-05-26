eb24b7c17a9330cb3315c019e5d34a46df65c561
cancdy <574318202@qq.com>
2021-05-26 11:11:13 +0800

    Initial commit

---
+++ src/main/java/com/xyf/controller/RoleController.java
@@ -0,0 +1,25 @@
+package com.xyf.controller;
+
+import com.xyf.domain.Role;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@RequestMapping("/role")
+public class RoleController {
+    private  RoleService roleService;
+
+    public void setRoleService(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @RequestMapping("/list")
+    public ModelAndView list(){
+        ModelAndView modelAndView=new ModelAndView();
+        List<Role> roleList=roleService.list();
+        modelAndView.addObject("roleList",roleList);
+        return modelAndView;
+
+    }
+}
